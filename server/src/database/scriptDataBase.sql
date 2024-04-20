8b8e7794c169da17c414d3225be162d080f47327
Matias Selaive <mselaivecode@gmail.com>
2024-04-20 13:33:59 -0400

    Insert table

--- server/src/database/scriptDataBase.sql
+++ server/src/database/scriptDataBase.sql
@@ -13,3 +13,22 @@ INSERT INTO login (username, password) VALUES
 ('user1', 'password1'),
 ('user2', 'password2'),
 ('user3', 'password3');
+
+-- Visitors
+
+	create table visitors(
+		name varchar(50) not null,
+		guests varchar(50) not null,
+		building int not null,
+		time varchar(10) not null,
+        primary key(name)
+	);
+    
+-- Sample data
+
+INSERT INTO visitors (name, guests, building, time) VALUES 
+('John Doe', '2', 101, '10:00 AM'),
+('Jane Smith', '1', 202, '11:30 AM'),
+('Michael Johnson', '3', 303, '2:00 PM'),
+('Emily Brown', '2', 404, '3:45 PM'),
+('David Wilson', '1', 303, '5:15 PM');
