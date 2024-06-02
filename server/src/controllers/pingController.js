417a19f09a25309ccde8f6d3404e57651ae600b6
Matias Selaive <mselaivecodes@gmail.com>
2024-06-02 00:25:35 -0400

    nuevas cosas

--- server/src/controllers/pingController.js
+++ server/src/controllers/pingController.js
@@ -1,16 +1,5 @@
 const connection = require('../models/db');
 
-module.exports.ping= (req, res) => {
-    const consult = 'SELECT * FROM login';
-
-    try{
-        connection.query(consult, (err, results) => {
-            console.log(results);
-            res.json(results);
-        });
-    }
-    catch(e){
-        
-    }
-
+module.exports.ping = (req, res) => {
+    res.json({message: 'pong'});
 };
