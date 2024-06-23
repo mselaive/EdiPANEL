04765837752166e62b60cb64d338dd159b0853c6
Matias Selaive <mselaive@alumnos.uai.cl>
2024-06-22 20:22:09 -0400

    notis estacionamientos

--- EdiPANEL/src/components/Headers/Header.js
+++ EdiPANEL/src/components/Headers/Header.js
@@ -1,9 +1,56 @@
 
-import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import { Card, CardBody, CardTitle, Container, Row, Col, Alert } from "reactstrap";
 import { useTranslation } from "react-i18next";
+import React, {useEffect, useState} from "react";
 
 const Header = () => {
   const { t } = useTranslation("global");
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertColor, setAlertColor] = useState('');
+  const [timeoutId, setTimeoutId] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    const checkNotifications = () => {
+      const notifications = JSON.parse(localStorage.getItem('notifications') || '[]');
+      const currentTime = new Date().getTime();
+
+      notifications.forEach(notification => {
+        // Ajusta esta condición según sea necesario
+        if (currentTime >= notification.notificationTime - (15 * 60 * 1000)) { // 15 minutos antes
+          setShowAlert(true); // Mostrar la alerta
+          setAlertMessage(`15 minutos para el siguiente vehiculo: ${notification.id}`); // Establecer el mensaje de la alerta
+        }
+      });
+    };
+
+    const intervalId = setInterval(checkNotifications, 60000); // Revisar cada minuto
+
+    return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar
+  }, []);
+
+
+  const showAlertWithTimeout = (message, color) => {
+      setAlertMessage(message);
+      setAlertColor(color);
+      setShowAlert(true);
+
+      // Cancelar el temporizador anterior
+      if (timeoutId) {
+          clearTimeout(timeoutId);
+      }
+
+      // Iniciar un nuevo temporizador
+      const newTimeoutId = setTimeout(() => {
+          setShowAlert(false);
+      }, 3000); // 3 segundos
+
+      // Guardar el identificador del nuevo temporizador
+      setTimeoutId(newTimeoutId);
+  };
+
+
+
   return (
     <>
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
@@ -126,9 +173,16 @@ const Header = () => {
                 </Card>
               </Col>
             </Row>
+            {showAlert && (
+              <Alert color="danger" className="mt-3"> 
+                {alertMessage}
+              </Alert>
+              )}
           </div>
         </Container>
       </div>
+
+      
     </>
   );
 };
